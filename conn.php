2eb6d806eeade9d8d59e8028b106f2211daaae9d
novelizza <novel.izza.fardana@gmail.com>
2021-06-12 16:02:27 +0700

    test read

--- conn.php
+++ conn.php
@@ -9,11 +9,11 @@ $pass = "25f0e2f18d8135840d766b608491ae982fb6267447b0a4d603d62fbc3964ef1e";
 $conn = "host={$host} port={$port} dbname={$dbname} user={$user} password={$pass}";
 $dbconn = pg_connect($conn);
 
-if($dbconn){
-    echo "KONEKSI BERHASIL";
-} else {
-    echo "KONEKSI GAGAL";
-}
+// if($dbconn){
+//     echo "KONEKSI BERHASIL";
+// } else {
+//     echo "KONEKSI GAGAL";
+// }
 
-echo "<hr />";
+// echo "<hr />";
 ?>
